94c291601cabf0f3b55d3d26be1d0f9ef12bde80
Martin Sivorn <martin.sivorn@digital.cabinet-office.gov.uk>
2019-01-08 10:03:00 +0000

    added some comments to the code

--- pwnapi.py
+++ pwnapi.py
@@ -4,23 +4,25 @@ import json
 import re
 import hashlib
 
-apilogger = logging.getLogger('pwndcheck')
-apilogger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+# create a logger
 logger = logging.getLogger(__name__)
+# set the log level to debug and above
 logger.setLevel(logging.DEBUG)
 
+# not going to log to file at the minute
 #handler = logging.FileHandler('logger.log')
 #handler.setLevel(logging.DEBUG)
 
-# create a logging format
 
 # add the handlers to the logger
 logging.getLogger().addHandler(logging.StreamHandler())
 
+# starting point for all the API calls
 baseURL = "https://haveibeenpwned.com/api/v2"
 
+# human readable error strings
 fourHundredString = "400 - Bad request - the account does not comply with an acceptable format (i.e. it's an empty string)"
 fourOThreeString = "403 - Forbidden - no user agent has been specified in the request"
 fourOFourString = "404 - Not found - the account could not be found and has therefore not been pwned"
@@ -28,11 +30,7 @@ fourTwentyNineString = "Rate limit exceeded, refer to acceptable use of the API:
 fiveHundredString = "A server error occurred on haveibeenpwned.com. Please try again later."
 emailFormatString = "The provided string is not an email address"
 
-
-HEADERS = {'user-agent': 'firebreak-testapp/haveibeenpwnd/ v0.1', 'api-version': 2}
-range_url = 'https://api.pwnedpasswords.com/range/{}'
-email_url = 'https://haveibeenpwned.com/api/v2/breachedaccount/{}'
-
+# instance of the pwndapi class that does all of the API work
 class pwndapi():
     '''
     Mandatory parameters:
@@ -79,6 +77,7 @@ class pwndapi():
 
         url = baseURL + "/breaches" + truncate + domain + unverified
         resp = self.get_resource(url)
+        # right now we're just printing this to the screen....
         print(resp)
 
 
